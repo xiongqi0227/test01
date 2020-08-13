582ee1c7be82dff5e6c7f9ab28bf2b43c4be0f7a
熊祺 <xiongqi0227@163.com>
2020-08-13 09:41:54 +0800

    day01_git 作者熊祺

---
+++ workspace_git/day01_git/src/main/java/com/czxy/GitApplication.java
@@ -0,0 +1,15 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by xiongqi.
+ *  2020-08-13
+ */
+@SpringBootApplication
+public class GitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitApplication.class,args);
+    }
+}
